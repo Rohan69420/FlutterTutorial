095659dd06e9c4107b09f1c0276633e340d64c0d
Rohan <rohan13maharjan@gmail.com>
2024-02-25 15:58:24 +0545

    Finally can talk to localhost

--- flutterapp/lib/pages/post_scan.dart
+++ flutterapp/lib/pages/post_scan.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 class PostScanPage extends StatelessWidget {
-  PostScanPage({Key? key, required this.picture, required this.imageUrl}) : super(key: key);
+  PostScanPage({Key? key, required this.picture, required this.imageUrl})
+      : super(key: key);
 
   final XFile picture;
   final String imageUrl;
@@ -23,28 +24,23 @@ class PostScanPage extends StatelessWidget {
               const SizedBox(height: 24),
               Text(picture.name),
 
-            //needs a button here to upload to firebase storage
-            Text("The Obtained Url is: '${imageUrl}'"),
-            
-            //debug button
-            ElevatedButton(onPressed: () {
-              //YAssssssssss the splot worked!
-              String parsedUrl = imageUrl.split('%2F')[1].toString();
-              print("The split url is: ${parsedUrl}");
+              //needs a button here to upload to firebase storage
+              Text("The Obtained Url is: '${imageUrl}'"),
 
-            }, child: const Text("Button for debug")),
+              //http get request code here
+              ElevatedButton(
+                onPressed: () async {
+                  //YAssssssssss the splot worked!
+                  String parsedUrl = imageUrl.split('%2F')[1].toString();
+                  print("The split url is: ${parsedUrl}");
 
-            //http get request code here
-            // ElevatedButton(onPressed: ()
-            // async {
-            //   String parsedUrl = imageUrl.split('%F').toString();
-
-            //   var url = Uri.parse('127.0.0.1/5000');
-            //   var response = await http.get(url);
-            //   print('Response status: ${response.statusCode}');
-            //   print('Response body: ${response.body}');
-            // },
-            // child: const Text("Load Results"),),
+                  var url = Uri.parse('http://10.0.2.2:5000/imgUrl');
+                  var response = await http.get(url);
+                  print('Response status: ${response.statusCode}');
+                  print('Response body: ${response.body}');
+                },
+                child: const Text("Load Results"),
+              ),
             ]),
           ],
         ),
