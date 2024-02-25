6750a41f9202ea3314cea61c230f394296b7fa94
Rohan <rohan13maharjan@gmail.com>
2024-02-25 19:07:25 +0545

    Page turned into stateful, url string conditioned to forward the filename, setState enabled to refresh the textbox with data

--- flutterapp/lib/pages/post_scan.dart
+++ flutterapp/lib/pages/post_scan.dart
@@ -5,12 +5,19 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-class PostScanPage extends StatelessWidget {
+class PostScanPage extends StatefulWidget {
   PostScanPage({Key? key, required this.picture, required this.imageUrl})
       : super(key: key);
 
   final XFile picture;
   final String imageUrl;
+
+  @override
+  State<PostScanPage> createState() => _PostScanPageState();
+}
+
+class _PostScanPageState extends State<PostScanPage> {
+  String NutritionDetails = '';
 
   @override
   Widget build(BuildContext context) {
@@ -20,22 +27,36 @@ class PostScanPage extends StatelessWidget {
         child: Column(
           children: [
             Column(mainAxisSize: MainAxisSize.min, children: [
-              Image.file(File(picture.path), fit: BoxFit.cover, width: 250),
+              Image.file(File(widget.picture.path),
+                  fit: BoxFit.cover, width: 250),
               const SizedBox(height: 24),
-              Text(picture.name),
+              Text(widget.picture.name),
 
               //needs a button here to upload to firebase storage
-              Text("The Obtained Url is: '${imageUrl}'"),
+              Text("The Obtained Url is: '${widget.imageUrl}'"),
+
+              const SizedBox(
+                height: 20,
+              ),
+
+              Text(" '${NutritionDetails}'"),
+
+              const SizedBox(
+                height: 20,
+              ),
 
               //http get request code here
               ElevatedButton(
                 onPressed: () async {
                   //YAssssssssss the splot worked!
-                  String parsedUrl = imageUrl.split('%2F')[1].toString();
-                  print("The split url is: ${parsedUrl}");
-
-                  var url = Uri.parse('http://10.0.2.2:5000/imgUrl');
+                  String forwardedUrl = 'http://10.0.2.2:5000/imgUrl?url=' +
+                      widget.imageUrl.split('%2F')[1].toString();
+                  print("The split url is: ${forwardedUrl}");
+                  var url = Uri.parse(forwardedUrl);
                   var response = await http.get(url);
+                  setState(() {
+                    NutritionDetails = response.body;
+                  });
                   print('Response status: ${response.statusCode}');
                   print('Response body: ${response.body}');
                 },
